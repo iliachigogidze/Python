b59b662cae4a551801c55b56081265808b065c98
Ilia Chigogidze <iliachigogidze@gmail.com>
2018-12-09 20:15:10 +0400

    matrix

--- Algorithms/MatMult.py
+++ Algorithms/MatMult.py
@@ -4,11 +4,11 @@ sheqmeni) da gamodis mati namravli.
 '''
 
 def mat_mult(mat1, mat2):
-    mat1_m = len(mat1)
-    mat1_n = len(mat1[0])
+    mat1_m = len(mat1) # row
+    mat1_n = len(mat1[0]) #column
     
-    mat2_m = len(mat2)
-    mat2_n = len(mat2[0])
+    mat2_m = len(mat2) # row
+    mat2_n = len(mat2[0]) # columns
     
     print('mat1_n', mat1_n, '  mat2_m: ', mat2_m)
     
@@ -21,18 +21,22 @@ def mat_mult(mat1, mat2):
     
     print(mult)
 
-    for i in range(mat1_m-1):
+    for i in range(mat1_m):
         print('i: ', i)
-        for j in range(mat2_n-1):
+        for j in range(mat2_n):
             print('j: ', j)
             #mult[i][j]
             total = 0
-            for k in range(mat1_n-1):
+            print('mat1_n-1: ',mat1_n)
+            for k in range(mat1_n):
                 print('k: ', k)
+                print('mat1[i][k]: ', mat1[i][k], 'mat2[k][i]: ',mat2[k][i])
                 total += mat1[i][k] * mat2[k][i]
+            print('total: ', total)
             mult[i][j] = total
+            print('MULT: ', mult)
     return mult 
 
 
-print(mat_mult([[1,1],[1,1]], [[2,2],[2,2]]))
+print('ANSWER: ', mat_mult([[2,2],[3,3]], [[5,5],[5,5]]))
     
